7d2c7bb7b8d0dcc7e5421c31ed0426c83d16d5f2
gregkristian <gkristianp@gmail.com>
2021-02-23 23:22:31 +0000

    Working form to search for ticker and show on home page

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 import yfinance as yf
 from forms import TickerForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'purupurupurupuru'
 
-def get_stock_info(ticker):
+# Get stock info
+# Return: panda dataframe containing price history. Empty dataframe if ticker not found
+def get_stock_history(ticker):
     ticker = yf.Ticker(ticker)
-    history = ticker.history(period="1mo", interval="1d") #return pandas dataframe
-    return history # return pandas DF
+    stock_history_df = ticker.history(period="1mo", interval="1d") #return pandas dataframe
+    return stock_history_df # return pandas DF
 
 # Page based on template
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    form = TickerForm()
+    form = TickerForm() 
+    app.logger.info('ticker data 1 %s', form.ticker.data)
     if form.validate_on_submit():
-        return redirect(url_for('home'))
-    return render_template('home.html', title='Register', form=form)
+        app.logger.info('ticker data 2 %s', form.ticker.data)
+        stock_history_df = get_stock_history(form.ticker.data)
+        is_ticker_found = not(stock_history_df.empty)
+
+        return render_template('home.html',
+                                form=form, ticker=form.ticker.data,
+                                is_ticker_found=is_ticker_found,
+                                tables=[stock_history_df.to_html(classes='data')], titles=stock_history_df.columns.values)                    
+
+    return render_template('home.html', form=form, ticker=form.ticker.data)
 
 @app.route("/about")
 def about():
@@ -25,8 +36,10 @@ def about():
 @app.route("/market")
 def market():
     ticker = "NDX"
-    stock_history_df = get_stock_info(ticker)
-    return render_template('market.html', ticker=ticker, tables=[stock_history_df.to_html(classes='data')], titles=stock_history_df.columns.values)
+    stock_history_df = get_stock_history(ticker)
+    return render_template('market.html', 
+                            ticker=ticker,
+                            tables=[stock_history_df.to_html(classes='data')], titles=stock_history_df.columns.values)
 
 if __name__ == '__main__':
     app.run(debug=True)
