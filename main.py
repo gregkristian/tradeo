0c0dd3d05c0e3a54787d1cbd8df86af714f3a49a
gregkristian <gkristianp@gmail.com>
2021-02-25 23:15:55 +0000

    Add plot for daily closing price

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, send_file
 import yfinance as yf
 from forms import TickerForm
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+from io import BytesIO
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'purupurupurupuru'
@@ -15,12 +20,14 @@ def get_stock_history(ticker):
 # Page based on template
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    form = TickerForm() 
+    form = TickerForm()
     app.logger.info('ticker data 1 %s', form.ticker.data)
     if form.validate_on_submit():
         app.logger.info('ticker data 2 %s', form.ticker.data)
         stock_history_df = get_stock_history(form.ticker.data)
         is_ticker_found = not(stock_history_df.empty)
+
+        # image = create_chart()
 
         return render_template('home.html',
                                 form=form, ticker=form.ticker.data,
@@ -29,6 +36,25 @@ def home():
 
     return render_template('home.html', form=form, ticker=form.ticker.data)
 
+# Plot stock history
+@app.route('/fig/<ticker>')
+def fig(ticker):
+    stock_history_df = get_stock_history(ticker)
+    print(stock_history_df.keys())
+    close_price = stock_history_df['Close'].tolist()
+    # TODO date not working yet. faulty dataframe?
+    # date = stock_history_df['Date'].tolist()
+
+    fig = Figure()
+    subplot = fig.subplots()
+    subplot.plot(close_price)
+    # subplot.xlabel(date)
+    img = BytesIO()
+    fig.savefig(img, format="png")
+    img.seek(0)
+    return send_file(img, mimetype='image/png')
+
+# Static about page
 @app.route("/about")
 def about():
     return render_template('about.html')
