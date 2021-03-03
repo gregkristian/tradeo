fc31798116c786f542cc0af0faa6e3686510e8d6
gregkristian <gkristianp@gmail.com>
2021-03-03 22:45:44 +0000

    Refactor image generation

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, redirect, send_file
 from forms import TickerForm, CryptoForm
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from io import BytesIO
 import random
 
 from util import *
@@ -14,32 +13,26 @@ app.config['SECRET_KEY'] = 'purupurupurupuru'
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = TickerForm()
-    app.logger.info('ticker data 1 %s', form.ticker.data)
+
     if form.validate_on_submit():
-        app.logger.info('ticker data 2 %s', form.ticker.data)
         stock_history_df = get_stock_history(form.ticker.data)
         is_ticker_found = not(stock_history_df.empty)
+
+        stock_history_df = get_stock_history(form.ticker.data)
+        close_price = stock_history_df['Close'].tolist()
+        # TODO date not working yet. faulty dataframe?
+        # date = stock_history_df['Date'].tolist()
+
+        stock_chart = create_plot(close_price)
 
         return render_template('home.html',
                                 form=form, ticker=form.ticker.data,
                                 is_ticker_found=is_ticker_found,
+                                image_file=stock_chart,
                                 tables=[stock_history_df.to_html(classes='data')], titles=stock_history_df.columns.values)
 
     return render_template('home.html', form=form, ticker=form.ticker.data)
 
-# Standalone function to generate image
-@app.route('/fig/<ticker>')
-def stock_chart(ticker):
-    stock_history_df = get_stock_history(ticker)
-    close_price = stock_history_df['Close'].tolist()
-    # TODO date not working yet. faulty dataframe?
-    # date = stock_history_df['Date'].tolist()
-
-    fig = create_plot(close_price)
-    img = BytesIO()
-    fig.savefig(img, format="png")
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
 
 # Page based on template
 @app.route("/crypto", methods=['GET', 'POST'])
@@ -51,8 +44,11 @@ def crypto():
         # is_data_found = not(data.empty)
         is_data_found = True
 
+        crpyto_chart = create_plot([3,1,5,1])
+
         return render_template('crypto.html',
                                 form=form, crypto=form.crypto.data,
+                                image_file=crpyto_chart,
                                 is_data_found=is_data_found)
 
     return render_template('crypto.html', form=form, crypto=form.crypto.data)
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
+from io import BytesIO
 from matplotlib.figure import Figure
+from flask import url_for
 import yfinance as yf
 
 # Get stock info
@@ -8,9 +10,18 @@ def get_stock_history(ticker):
     stock_history_df = ticker.history(period="1mo", interval="1d")
     return stock_history_df
 
-# Create a plot from 1 dimensional data
+# Create a plot from 1 dimensional list, return image url
+# TODO accept panda dataframe instead
 def create_plot(data):
+    print(data)
     fig = Figure()
     subplot = fig.subplots()
     subplot.plot(data)
-    return fig
+
+    img = BytesIO()
+    fig.savefig('static/image.png', format="png", dpi=200)
+    # TODO avoid cache
+    img.seek(0)
+    image_url = url_for('static', filename = 'image.png')
+
+    return image_url
