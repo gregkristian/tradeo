a69a39fc6c35058a218ee188697ede9fa51ac66b
gregkristian <gkristianp@gmail.com>
2021-04-12 23:22:50 +0200

    Add dataset

--- sandbox/crypto.py
+++ sandbox/crypto.py
@@ -1,19 +1,33 @@
-import sys
-import pprint
+import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+
 from util import save_plot
 
-# Read training set
-with open('trainingset.txt', 'r') as f:
-    data = f.read().splitlines()
+# Read csv. Use Open and High (col 1 & 2) to predict Close price (col 4). Normalize
+dataset_df = pd.read_csv("dataset.csv")
 
-print(len(data))
-print(type(data))
+print(dataset_df.head())
 
+training_set = dataset_df.iloc[:800, 1:2].values
+test_set = dataset_df.iloc[800:, 1:2].values
+
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_norm = sc.fit_transform(training_set)
+test_set_norm = sc.fit_transform(test_set)
+
+#print(training_set_norm.head())
+#print(test_set_norm.head())
+
+#data = data[:1000]
+# print(len(data))
+
+#plt.plot(data)
+#plt.show()
 # save_plot('img1.png', data)
 
-data_normalized = preprocessing.MinMaxScaler().fit_transform(data)
+# data_normalized = preprocessing.
 
-print(len(data_normalized))
-print(type(data_normalized))
+# print(len(data_normalized))
+# print(type(data_normalized))
--- sandbox/fetch_data.py
+++ sandbox/fetch_data.py
@@ -1,15 +1,18 @@
 from binance.client import Client
 
 # Fetch crypto historical data and write to file for training
-with open('keyfile.conf', "r") as f1:
-    KEYS = f1.read().splitlines()
+with open('keyfile.conf', "r") as keyfile:
+    KEYS = keyfile.read().splitlines()
 
 client = Client(KEYS[0], KEYS[1])
 klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Jan, 2005")
-daily_close_prices = [int(float(el[4])) for el in klines]
 
-# Write to file
-with open('trainingset.txt', 'w+') as f2:
-    for items in daily_close_prices:
-        f2.write('%s\n' %items)
+# Add header, convert to int, write to csv
+with open('dataset.csv', 'w+') as f:
+    header_str = "Open time,Open,High,Low,Close,Volume,Close time,Quote asset volume,Nr of trades,Taker buy base asset volume,Taker buy quote asset volume"
+    f.write('%s\n' % header_str)
+    for items in klines:
+        del items[-1] # rm last column
+        items_str = ','.join(map(str, items))
+        f.write('%s\n' % items_str)
     print("File written")
